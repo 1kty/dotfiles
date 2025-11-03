ffc1ec1cefdc1d4b51ba6fc71d18f35e8c45bf34
1kty <210384061+1kty@users.noreply.github.com>
2025-11-03 22:39:15 +0100

    refactor: replace old config with new setup

--- .config/waybar/scripts/btop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if hyprctl clients | grep -q "title: floatterm"; then
-	pkill -f "foot.*--title floatterm";
-	exit 0
-fi
-
-foot --title floatterm bash -c "btop"
--- .zprofile
+++
@@ -1,4 +0,0 @@
-if [[ -z "$DISPLAY" && -z "$WAYLAND_DISPLAY" && $(tty) == /dev/tty1 ]]; then
-    exec Hyprland
-fi
-
