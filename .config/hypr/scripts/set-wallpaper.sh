fefa1fbb20909963dd7235becb6d5a5ad9d304a3
1kty <210384061+1kty@users.noreply.github.com>
2025-11-04 02:25:05 +0100

    fix(pywal): add --cols16 darken option, fix typo

--- .config/hypr/scripts/set-wallpaper.sh
+++ .config/hypr/scripts/set-wallpaper.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -eu
 
-WALL=$1
+WALL=$(realpath "$1")
+
 echo "setting $WALL as wallpaper"
 hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$WALL"
 hyprctl hyprpaper wallpaper ", $WALL"
-echo "set $WALL as wallpaper sucessfuly"
+cp -f "$WALL" "$HOME/.config/background"
+echo "set $WALL as wallpaper sucessfully"
 
 if command -v wal >/dev/null 2>&1; then
     echo "applying pywal colors..."
-    wal -i "$WALL"
+    wal -i "$WALL" --cols16 darken
     echo "pywal applied successfully"
     "$HOME/.config/mako/update-colors.sh"
 else
