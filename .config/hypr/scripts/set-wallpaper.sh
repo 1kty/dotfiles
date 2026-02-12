afb79ffbc1e1ab259e88704ba9b38d4e6ddcf293
1kty <210384061+1kty@users.noreply.github.com>
2026-02-12 14:46:28 +0100

    feat(set-wallpaper.sh): allow changing noctalia-managed wallpaper

--- .config/hypr/scripts/set-wallpaper.sh
+++ .config/hypr/scripts/set-wallpaper.sh
@@ -7,6 +7,7 @@ if [ $# -lt 1 ]; then
 fi
 
 WALL=$(realpath "$1")
+MONITOR=eDP-1
 
 if [ ! -f "$WALL" ]; then
     echo "Error: file not found: $WALL"
@@ -14,18 +15,7 @@ if [ ! -f "$WALL" ]; then
 fi
 
 echo "Setting $WALL as wallpaper..."
-swww img --transition-type=fade --transition-duration=2 "$WALL"
+qs -c noctalia-shell ipc call wallpaper set $WALL $MONITOR
 cp -f "$WALL" "$HOME/.config/background"
 echo "Set $WALL as wallpaper successfully"
 
-if command -v wal >/dev/null 2>&1; then
-    echo "Applying pywal colors..."
-    if ! wal -i "$WALL" --cols16 darken "${@:2}" 2>/dev/null; then
-        echo "Primary wal backend failed, trying colorthief..."
-        wal -i "$WALL" --cols16 darken --backend colorthief "${@:2}"
-    fi
-    echo "Pywal applied successfully"
-    "$HOME/.config/mako/update-colors.sh"
-else
-    echo "Pywal not installed, skipping"
-fi
